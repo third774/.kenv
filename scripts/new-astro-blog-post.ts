96a5dde57a2aa6e86963f0432d33182184af8eed
Kevin Kipp <kevin.kipp@gmail.com>
2023-12-15 12:56:19 -0600

    Update new blog post script

--- scripts/new-astro-blog-post.ts
+++ scripts/new-astro-blog-post.ts
@@ -1,28 +1,37 @@
 // Name: New Astro Blog Post
 
-import "@johnlindquist/kit";
+import '@johnlindquist/kit';
 
 const title = await arg("What's the title of the post?");
 const description = await arg("What's the description of the post?");
-const slug = title.toLowerCase().split(" ").join("-");
+const slug = title.toLowerCase().split(' ').join('-');
 
-const blogCollectionPath = await env("ASTRO_BLOG_COLLECTION_PATH");
+const astroBlogProjectPath = await env('ASTRO_BLOG_PROJECT_PATH');
+const blogCollectionPath = await env(
+  'ASTRO_BLOG_COLLECTION_PATH',
+  async () => 'src/content/blog',
+);
 
 const content = `---
 {
   "title": "${title}",${
-  description ? `\n  "description": "${description}",` : ""
-}
+    description ? `\n  "description": "${description}",` : ''
+  }
   "published": "${new Date().toISOString()}"
 }
 ---
 
 `;
 
-const filePath = `${blogCollectionPath}/${slug}.md`;
-
+const filePath = `${astroBlogProjectPath}/${blogCollectionPath}/${slug}.md`;
 const editedContent = await editor(content);
 
-await writeFile(filePath, editedContent);
+console.log({
+  filePath,
+  astroBlogProjectPath,
+  blogCollectionPath,
+});
 
+await writeFile(filePath, editedContent);
+await exec(`/opt/homebrew/bin/code ${astroBlogProjectPath}`);
 await exec(`/opt/homebrew/bin/code ${filePath}`);
